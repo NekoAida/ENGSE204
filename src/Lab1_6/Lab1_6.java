9355ec56109a6afa9a679849649e1690d132bcae
NekoAida <toffyrock10@gmail.com>
2025-11-22 17:21:14 +0700

    Lab1.6 Fix

--- src/Lab1_6/Lab1_6.java
+++ src/Lab1_6/Lab1_6.java
@@ -5,12 +5,11 @@ public class Lab1_6 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter Number: ");
+        System.out.print("Enter Number: ");
         int number = input.nextInt();
 
         for (int i = 1; i <= 12; i++) {
-            int result = number * i;
-            System.out.println(number + "x" + i + "=" + result);
+            System.out.println(number + "x" + i + "=" + (number * i));
         }
 
     }
