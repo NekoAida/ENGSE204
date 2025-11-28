ae5100d6298ff59628e40aef779e3f87ef90af90
NekoAida <toffyrock10@gmail.com>
2025-11-28 18:28:21 +0700

    Lab2.1

--- src/Lab2_1/Lab2_1.java
+++ src/Lab2_1/Lab2_1.java
@@ -1,27 +1,39 @@
 package Lab2_1;
 import java.util.Scanner;
 
-public class Student {
-
+class Student {
     private String studentID;
     private String name;
-    private double gpa;
-    private  int year;
-    private String major;
-    private  boolean isActive;
 
-    public Student(String id, String n, String m, int y) {
+    public Student(String id, String n) {
         this.studentID = id;
         this.name = n;
-        this.major = m;
-        this.year = y;
-        this.gpa = 0.0;
-        this.isActive = true;
+    }
+
+    public String getStudentID() {
+        return this.studentID;
+    }
+
+    public String getName() {
+        return this.name;
     }
 }
 
 public class Lab2_1 {
-    public static void main(String[] args);
-    Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
+        System.out.print("Enter StudentID: ");
+        String studentID = input.nextLine();
+
+        System.out.print("Enter Student Name: ");
+        String name = input.nextLine();
+        
+        Student student = new Student(studentID, name);
+        
+        System.out.println(student.getStudentID());
+        System.out.println(student.getName());
+        
+        input.close();
+    }
 }
