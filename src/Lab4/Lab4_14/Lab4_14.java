380baee1eda47ed3a3a95ec0e45128b5a049448e
NekoAida <toffyrock10@gmail.com>
2025-12-16 18:17:00 +0700

    Lab4.14

---
+++ src/Lab4/Lab4_14/Lab4_14.java
@@ -0,0 +1,55 @@
+package Lab4.Lab4_14;
+import java.util.Scanner;
+
+class ScoreBoard {
+    private final String gameName;
+    private final int[] scores;
+
+    public ScoreBoard(String gameName, int[] scores) {
+        this.gameName = gameName;
+        this.scores = scores;
+    }
+
+    public ScoreBoard(ScoreBoard other) {
+        this.gameName = other.gameName;
+        this.scores = new int[other.scores.length];
+        for (int i = 0; i < other.scores.length; i++) {
+            this.scores[i] = other.scores[i];
+        }
+    }
+
+    public void displayScores() {
+        System.out.println(this.gameName + ": " + this.scores[0] + ", " + this.scores[1] + ", " + this.scores[2]);
+    }
+}
+
+public class Lab4_14 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter Game Name: ");
+        String gameName = input.nextLine();
+
+        System.out.print("Enter Score 1: ");
+        int s1 = input.nextInt();
+        System.out.print("Enter Score 2: ");
+        int s2 = input.nextInt();
+        System.out.print("Enter Score 3: ");
+        int s3 = input.nextInt();
+
+        System.out.print("Enter Modified Score 1: ");
+        int modVal = input.nextInt();
+
+        int[] originalScores = {s1, s2, s3};
+
+        ScoreBoard sb1 = new ScoreBoard(gameName, originalScores);
+        ScoreBoard sb2 = new ScoreBoard(sb1);
+
+        originalScores[0] = modVal;
+
+        sb1.displayScores();
+        sb2.displayScores();
+
+        input.close();
+    }
+}
